4f0793269c1d3798c54cabb903ce168d0e94ca46
Laura <lauratelqiu@gmail.com>
2022-12-14 21:02:33 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css"
 import NavBar from "./screens/navigationBar/NavBar";
 
 function App() {
--- src/screens/navigationBar/NavBar.js
+++ src/screens/navigationBar/NavBar.js
@@ -1,29 +1,28 @@
 import React from "react";
+import "./navigation.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { navigationLinks } from "../../helpers/navigationLinks";
 
 function createLinks() {
   return navigationLinks.map((e, idx) => (
-    <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
-  ))
+    <Nav.Link key={idx} href={e.ref}>
+      {e.name}
+    </Nav.Link>
+  ));
 }
 function NavBar() {
   return (
-    <div>
-      <div id="home">
-        <Navbar expand='md'>
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav>
-            <Navbar.Collapse>
-              {createLinks()}
-
-            </Navbar.Collapse>
+    <div id="home">
+      <Navbar className="navigation_contanier" expand="md">
+        <Navbar.Brand style={{ marginLeft: "1rem" }} href="#home">Laura Telqiu</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse style={{ justifyContent: "flex-end", marginRight: "1rem", borderColor: "none" }}>
+          <Nav className="links" style={{ margin: "0 1rem" }}>
+            {createLinks()}
           </Nav>
-
-        </Navbar>
-      </div>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
